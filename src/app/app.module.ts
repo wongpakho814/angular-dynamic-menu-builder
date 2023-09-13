bf6e8a11f3a6efa5b01dd97ae2e48bea25116eba
wongpakho814 <wongpakho814@gmail.com>
2023-09-13 16:51:44 +1000

    Fixed styling of switch role button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  menuDict = {
+    menu_1: ["menu_1_1", "menu_1_2"], 
+    menu_2: ["menu_2_1", "menu_2_2", "menu_2_3", "menu_2_4"],
+    menu_3: ["menu_3_1", "menu_3_2"],
+    menu_4: ["menu_4_1", "menu_4_2", "menu_4_3"], 
+    menu_5: ["menu_5_1", "menu_5_2", "menu_5_3"],
+    menu_6: ["menu_6_1", "menu_6_2"],
+    menu_7: ["menu_7_1", "menu_7_2", "menu_7_3"]
+  };
+
+  permissionDict = {
+    PERMISSION_1: ["menu_5_2", "menu_5_3"],
+    PERMISSION_2: ["menu_7_1"],
+    PERMISSION_3: ["menu_2_1"],
+    PERMISSION_4: ["menu_2_2", "menu_6_2"],
+    PERMISSION_6: ["menu_1_1", "menu_1_2"],
+    PERMISSION_8: ["menu_3_1"],
+    PERMISSION_12: ["menu_7_3"],
+    PERMISSION_13: ["menu_5_1"],
+    PERMISSION_14: ["menu_2_3"],
+    PERMISSION_15: ["menu_2_4"],
+    PERMISSION_16: ["menu_7_2"],
+    PERMISSION_17: ["menu_6_1"],
+    PERMISSION_18: ["menu_4_2"],
+    PERMISSION_19: ["menu_4_3"],
+    PERMISSION_26: ["menu_4_1"],
+    PERMISSION_28: ["menu_3_2"],
+  };
+
+  systemStatusDict = {
+    SYSTEM_CONTROL_1: ["menu_6_1", "menu_6_2"],
+    SYSTEM_CONTROL_2: ["menu_1_1", "menu_1_2"],
+    SYSTEM_CONTROL_3: ["menu_2_1"],
+    SYSTEM_CONTROL_4: ["menu_2_3"],
+    SYSTEM_CONTROL_5: ["menu_2_2"],
+    SYSTEM_CONTROL_6: ["menu_7_1"],
+    SYSTEM_CONTROL_7: ["menu_7_2"],
+    SYSTEM_CONTROL_8: ["menu_3_2"],
+    SYSTEM_CONTROL_9: ["menu_4_1"],
+    SYSTEM_CONTROL_10: ["menu_7_3"],
+    SYSTEM_CONTROL_12: ["menu_2_4"],
+    SYSTEM_CONTROL_14: ["menu_5_1"],
+    SYSTEM_CONTROL_15: ["menu_5_2"],
+    SYSTEM_CONTROL_16: ["menu_5_3"],
+  };
+
+  roleDropdown() {
+  }
+}
